e645fc0f5b5f8be446e49994be56e513e315d24c
perik <perik0917@gmail.com>
2023-10-17 09:46:17 +0200

    első szinkron

--- beadando.py
+++ beadando.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from datetime import datetime
 import re
+from tkcalendar import Calendar
 
 class RegistrationProgram:
     def __init__(self, parent):
@@ -30,85 +31,100 @@ class RegistrationProgram:
         label_vezeteknev.grid(row=0, column=0, sticky="w")
         self.entry_vezeteknev = ttk.Entry(self.parent)
         self.entry_vezeteknev.grid(row=0, column=1)
+        self.entry_vezeteknev.config(justify="center")  # Középre igazítás
 
         label_keresztnev = ttk.Label(self.parent, text="Keresztnév:")
         label_keresztnev.grid(row=1, column=0, sticky="w")
         self.entry_keresztnev = ttk.Entry(self.parent)
         self.entry_keresztnev.grid(row=1, column=1)
+        self.entry_keresztnev.config(justify="center")  # Középre igazítás
 
         label_szuletesi_ido = ttk.Label(self.parent, text="Születési idő:")
-        (label_szuletesi_ido.grid(row=2, column=0, sticky="w"))
-
-        self.entry_szuletesi_ido = ttk.Entry(self.parent)
-        self.entry_szuletesi_ido.grid(row=2, column=1)
+        label_szuletesi_ido.grid(row=2, column=0, sticky="w")
 
         calendar_button = ttk.Button(self.parent, text="Választás", command=self.select_date)
-        calendar_button.grid(row=3, column=0, columnspan=2)
+        calendar_button.grid(row=2, column=1)
 
         label_szuletesi_hely = ttk.Label(self.parent, text="Születési hely:")
-        label_szuletesi_hely.grid(row=4, column=0, sticky="w")
+        label_szuletesi_hely.grid(row=3, column=0, sticky="w")
         self.entry_szuletesi_hely = ttk.Entry(self.parent)
-        self.entry_szuletesi_hely.grid(row=4, column=1)
+        self.entry_szuletesi_hely.grid(row=3, column=1)
+        self.entry_szuletesi_hely.config(justify="center")  # Középre igazítás
 
         label_cim = ttk.Label(self.parent, text="Lakcím:")
-        label_cim.grid(row=5, column=0, sticky="w")
+        label_cim.grid(row=4, column=0, sticky="w")
 
         label_lakohely = ttk.Label(self.parent, text="Lakóhely:")
-        label_lakohely.grid(row=6, column=0, sticky="w")
+        label_lakohely.grid(row=5, column=0, sticky="w")
         self.entry_lakohely = ttk.Entry(self.parent)
-        self.entry_lakohely.grid(row=6, column=1)
+        self.entry_lakohely.grid(row=5, column=1)
+        self.entry_lakohely.config(justify="center")  # Középre igazítás
 
         label_utca = ttk.Label(self.parent, text="Utca:")
-        label_utca.grid(row=7, column=0, sticky="w")
+        label_utca.grid(row=6, column=0, sticky="w")
         self.entry_utca = ttk.Entry(self.parent)
-        self.entry_utca.grid(row=7, column=1)
+        self.entry_utca.grid(row=6, column=1)
+        self.entry_utca.config(justify="center")  # Középre igazítás
 
         label_hazszam = ttk.Label(self.parent, text="Házszám:")
-        label_hazszam.grid(row=8, column=0, sticky="w")
+        label_hazszam.grid(row=7, column=0, sticky="w")
         self.entry_hazszam = ttk.Entry(self.parent)
-        self.entry_hazszam.grid(row=8, column=1)
+        self.entry_hazszam.grid(row=7, column=1)
+        self.entry_hazszam.config(justify="center")  # Középre igazítás
 
         label_taj = ttk.Label(self.parent, text="TAJ-szám (000-000-000):")
-        label_taj.grid(row=9, column=0, sticky="w")
+        label_taj.grid(row=8, column=0, sticky="w")
         self.entry_taj = ttk.Entry(self.parent)
-        self.entry_taj.grid(row=9, column=1)
+        self.entry_taj.grid(row=8, column=1)
+        self.entry_taj.config(justify="center")  # Középre igazítás
 
         regisztracio_button = ttk.Button(self.parent, text="Regisztráció", command=self.register)
-        regisztracio_button.grid(row=10, column=0, columnspan=2)
+        regisztracio_button.grid(row=9, column=0, columnspan=2)
 
+        self.year_var = tk.StringVar()
+        self.month_var = tk.StringVar()
+        self.day_var = tk.StringVar()
         self.create_calendar_widgets()
 
     def create_calendar_widgets(self):
         year_label = ttk.Label(self.parent, text="Év:")
         year_label.grid(row=2, column=2, sticky="w")
-        year_entry = ttk.Entry(self.parent, textvariable=self.year_var)
-        year_entry.grid(row=2, column=3)
+        self.year_entry = ttk.Entry(self.parent, textvariable=self.year_var)
+        self.year_entry.grid(row=2, column=3)
+        self.year_entry.config(justify="center")  # Középre igazítás
 
         month_label = ttk.Label(self.parent, text="Hónap:")
         month_label.grid(row=2, column=4, sticky="w")
-        month_entry = ttk.Entry(self.parent, textvariable=self.month_var)
-        month_entry.grid(row=2, column=5)
+        self.month_entry = ttk.Entry(self.parent, textvariable=self.month_var)
+        self.month_entry.grid(row=2, column=5)
+        self.month_entry.config(justify="center")  # Középre igazítás
 
         day_label = ttk.Label(self.parent, text="Nap:")
         day_label.grid(row=2, column=6, sticky="w")
-        day_entry = ttk.Entry(self.parent, textvariable=self.day_var)
-        day_entry.grid(row=2, column=7)
+        self.day_entry = ttk.Entry(self.parent, textvariable=self.day_var)
+        self.day_entry.grid(row=2, column=7)
+        self.day_entry.config(justify="center")  # Középre igazítás
 
     def select_date(self):
-        try:
-            year = int(self.year_var.get())
-            month = int(self.month_var.get())
-            day = int(self.day_var.get())
-            self.selected_date = datetime(year, month, day)
-            self.entry_szuletesi_ido.delete(0, tk.END)
-            self.entry_szuletesi_ido.insert(0, self.selected_date.strftime('%Y-%m-%d'))
-        except ValueError:
-            messagebox.showerror("Hiba", "Érvénytelen dátum!")
+        self.cal = Calendar(self.parent)
+        self.cal.grid(row=3, column=0, columnspan=2)
+        self.cal.bind("<<CalendarSelected>>", self.update_selected_date)
+
+    def update_selected_date(self, event):
+        selected_date_str = self.cal.get_date()
+        selected_date = datetime.strptime(selected_date_str, '%x')
+        year, month, day = selected_date.year, selected_date.month, selected_date.day
+        self.year_var.set(year)
+        self.month_var.set(month)
+        self.day_var.set(day)
+        self.cal.grid_remove()  # A naptár eltűntetése a kiválasztás után
 
     def register(self):
         first_name = self.entry_vezeteknev.get()
         last_name = self.entry_keresztnev.get()
-        birthdate = self.entry_szuletesi_ido.get()
+        year = self.year_var.get()
+        month = self.month_var.get()
+        day = self.day_var.get()
         birthplace = self.entry_szuletesi_hely.get()
         city = self.entry_lakohely.get()
         street = self.entry_utca.get()
@@ -123,6 +139,12 @@ class RegistrationProgram:
             messagebox.showerror("Hiba", "Házszám csak számokat tartalmazhat!")
             return
 
+        # Most a vizsgálat csak akkor dob hibát, ha valamelyik mező nincs kitöltve
+        if not first_name or not last_name or not year or not month or not day or not birthplace or not city or not street or not house_number or not taj:
+            messagebox.showerror("Hiba", "Minden mező kitöltése kötelező!")
+            return
+
+        birthdate = f"{year}-{month}-{day}"  # Születési dátum összeállítása
         registration_data = f"Név: {first_name} {last_name}, Születési dátum: {birthdate}, Születési hely: {birthplace}, Lakcím: {city}, {street} {house_number}, TAJ-szám: {taj}"
 
         with open("registered_people.txt", "a") as file:
